94de1f8a4b494d775029530ce39c4f0bbd1d2da5
KindaMe <kalinowski1999@gmail.com>
2024-01-13 18:18:38 +0100

    Init commit.

---
+++ VoiceBot/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace VoiceBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private String[] _grammarFile = File.ReadAllLines("grammar.txt");
+        private String[] _responseFile = File.ReadAllLines("response.txt");
+
+        private SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
+
+        private Choices _grammarList = new Choices();
+        private SpeechRecognitionEngine _speechRecognitionEngine = new SpeechRecognitionEngine();
+        
+        public MainWindow()
+        {
+            _grammarList.Add(_grammarFile);
+            Grammar grammar = new Grammar(new GrammarBuilder(_grammarList));
+
+            try
+            {
+                _speechRecognitionEngine.RequestRecognizerUpdate();
+                _speechRecognitionEngine.LoadGrammar(grammar);
+                _speechRecognitionEngine.SpeechRecognized += SpeechRecognitionEngineOnSpeechRecognized;
+                _speechRecognitionEngine.SetInputToDefaultAudioDevice();
+                _speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+
+            _speechSynthesizer.SelectVoiceByHints(VoiceGender.Female);
+            
+            InitializeComponent();
+        }
+
+        public void Say(String text)
+        {
+            _speechSynthesizer.SpeakAsync(text);
+        }
+
+        private void SpeechRecognitionEngineOnSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String result = e.Result.Text;
+
+            int resp = Array.IndexOf(_grammarFile, result);
+            
+            Say(_responseFile[resp]);
+        }
+    }
+}
