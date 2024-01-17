76dca27e8b8274690b8130019361b3a66dfb0c8b
KindaMe <kalinowski1999@gmail.com>
2024-01-17 20:34:19 +0100

    Add new commands and fix some issues.

--- VoiceBot/MainWindow.xaml.cs
+++ VoiceBot/MainWindow.xaml.cs
@@ -5,23 +5,20 @@ using System.Speech.Synthesis;
 
 namespace VoiceBot
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
-        private String[] _grammarFile = File.ReadAllLines("grammar.txt");
-        private String[] _responseFile = File.ReadAllLines("response.txt");
+        private readonly string[] _grammarFile = File.ReadAllLines("grammar.txt");
+        private readonly string[] _responseFile = File.ReadAllLines("response.txt");
 
-        private SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
+        private readonly SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
 
-        private Choices _grammarList = new Choices();
-        private SpeechRecognitionEngine _speechRecognitionEngine = new SpeechRecognitionEngine();
+        private readonly Choices _grammarList = new Choices();
+        private readonly SpeechRecognitionEngine _speechRecognitionEngine = new SpeechRecognitionEngine();
 
         public MainWindow()
         {
             _grammarList.Add(_grammarFile);
-            Grammar grammar = new Grammar(new GrammarBuilder(_grammarList));
+            var grammar = new Grammar(new GrammarBuilder(_grammarList));
 
             try
             {
@@ -41,26 +38,35 @@ namespace VoiceBot
             InitializeComponent();
         }
 
-        public void Say(String text)
+        private void Say(string text)
         {
             _speechSynthesizer.SpeakAsync(text);
         }
 
         private void SpeechRecognitionEngineOnSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            String result = e.Result.Text;
+            var result = e.Result.Text;
 
-            int resp = Array.IndexOf(_grammarFile, result);
+            var resp = Array.IndexOf(_grammarFile, result);
 
             if (_responseFile[resp].IndexOf('*') == 0)
             {
-                if (result.Contains("time"))
+                switch (_responseFile[resp])
                 {
-                    Say(DateTime.Now.ToString(@"hh\:mm tt"));
-                }
-                else if(result.Contains("date"))
-                {
-                    Say(DateTime.Now.ToString(@"dd MMMM yyyy"));
+                    case "*time":
+                        Say(DateTime.Now.ToString(@"hh\:mm tt"));
+                        break;
+                    case "*date":
+                        Say(DateTime.Now.ToString(@"dd MMMM yyyy"));
+                        break;
+                    case "*dark mode":
+                        Background = System.Windows.Media.Brushes.Black;
+                        centerLabel.Foreground = System.Windows.Media.Brushes.White;
+                        break;
+                    case "*light mode":
+                        Background = System.Windows.Media.Brushes.White;
+                        centerLabel.Foreground = System.Windows.Media.Brushes.Black;
+                        break;
                 }
             }
             else
